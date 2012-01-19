806a9bf1d813feeba1f9e608101fb1db8891d399
Thomas Poetsch <thp@comnets.uni-bremen.de>
2012-01-19 14:30:39 +0100

    added missing xml files for menu

---
+++ AndroidCoAP/src/de/tzi/coap/IPv6AddressesHelper.java
@@ -0,0 +1,81 @@
+package de.tzi.coap;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.TextView;
+
+public class IPv6AddressesHelper extends AsyncTask<TextView, String, String> {
+	TextView ipv6Address;
+
+	@Override
+	protected String doInBackground(TextView... params) {
+		ipv6Address = params[0];
+		onProgressUpdate("Getting IPv6");
+		Boolean ipv6Available = false;
+		try {
+			// Executes the command.
+			// Process process = Runtime.getRuntime().exec("cat /proc/net/arp");
+			// Process process =
+			// Runtime.getRuntime().exec("ip -f inet6 addr show wlan0 scope global");
+			Process process = Runtime.getRuntime().exec(
+					"cat /proc/net/if_inet6");
+
+			// Reads stdout
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					process.getInputStream()));
+
+			StringBuilder log = new StringBuilder();
+			String line;
+
+			while ((line = reader.readLine()) != null) {
+
+				String[] buffer;
+				buffer = line.split(" ");
+				if (!(line.startsWith("0000") || line.startsWith("fe"))) {
+					StringBuffer output = new StringBuffer(buffer[0]);
+
+					int i = 4;
+					while (output.length() < 39) {
+						output.insert(i, ":");
+						i = i + 5;
+					}
+					log.append("\n" + output);
+					ipv6Available = true;
+				}
+
+			}
+			Log.d("IPHELPER", "" + log);
+
+			reader.close();
+
+			// Waits for the command to finish.
+			process.waitFor();
+
+			// return output.toString();
+			if (ipv6Available)
+				return log.toString();
+			else
+				return "IPv6 not available...";
+
+		} catch (IOException e) {
+			// do nothing
+		} catch (InterruptedException e) {
+			// do nothing
+		} catch (RuntimeException re) {
+			// do nothing
+		}
+		return "IPv6 not available...";
+	}
+
+	protected void onProgressUpdate(String... progress) {
+		ipv6Address.setText(progress[0]);
+	}
+
+	protected void onPostExecute(String result) {
+		ipv6Address.setText(result);
+	}
+
+}
---
+++ AndroidCoAP/src/de/tzi/coap/SettingsActivity.java
@@ -0,0 +1,56 @@
+package de.tzi.coap;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SettingsActivity extends Activity {
+	SharedPreferences settings;
+	EditText resourcesText;
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+//		super.onCreate(savedInstanceState);
+//		setContentView(R.layout.settings);
+//		Button saveButton = (Button) findViewById(R.id.btn_save);
+//		Button cancelButton = (Button) findViewById(R.id.btn_cancel);  
+//		resourcesText = (EditText)findViewById(R.id.editTextResources);
+//		settings = getSharedPreferences(CoAPClient.PREFS_NAME, 0);
+//		resourcesText.setText(settings.getString("uris", ""));
+//
+//		saveButton.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				//	save stuff
+//				SharedPreferences.Editor editor = settings.edit();
+//				editor.putString("uris", resourcesText.getText().toString());
+//				editor.commit();
+//				finish();
+//			}
+//
+//		});
+//
+//		cancelButton.setOnClickListener(new OnClickListener() {
+//
+//			@Override
+//			public void onClick(View v) {
+//				finish();
+//			}
+//
+//		});
+
+	}
+
+
+	@Override
+	protected void onDestroy() {
+		//			Log.d("CoAP", " Destroy SettingsActivity...");
+		super.onDestroy();
+
+	}
+}
